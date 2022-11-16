f418b00c80753303deb7f62977bd28da79bc1fa9
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-16 13:36:24 +0100

    Craezione store.js

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
 <script>
+import { store } from "./assets/js/store";
+import axios from "axios";
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+
+    }
+
+  }, methods: {
+    callApi(url) {
+      axios.get(url).then(response => {
+        console.log(response.data);
+        this.store.results = response.data
+      })
+    }
+
+  },
+  mounted() {
+    this.callApi(this.store.API_url)
+  }
+}
+
 
 </script>
 
